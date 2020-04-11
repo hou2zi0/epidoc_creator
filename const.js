b5f57d89dd48d1292118838f197f3a1de84c5e1a
Max Grüntgens <max.gruentgens@adwmainz.de>
2020-04-11 20:02:38 +0200

    Initialized repo

---
+++ const.js
@@ -0,0 +1,63 @@
+const CONST = {
+    "titleStmt": `<div id="titleStmt" class="subsection">
+                    <p><label for="title">Title:</label> <input type="text" value="" placeholder="Write something..." id="title" name="title"></p>
+                    <p><label for="editor">Editor:</label> <input type="text" value="" placeholder="Write something..." id="editor" name="editor"></p>
+                </div>`,
+    "publicationStmt": `<div id="publicationStmt" class="subsection">
+                            <p><label for="authority">Authority:</label> <input type="text" value="" placeholder="Write something..." id="authority" name="authority"></p>
+                            <p><label for="idno_file">File id:</label> <input type="text" value="" placeholder="Write something..." id="idno_file" name="idno_file"></p>
+                        </div>`,
+    "msIdentifier": `<div id="msIdentifier" class="subsection">
+                        <p><label for="repository">Repository:</label> <input type="text" value="" placeholder="Write something..." id="repository" name="repository"></p>
+                        <p><label for="idno">Inventory number:</label> <input type="text" value="" placeholder="Write something..." id="idno" name="idno"></p>
+                    </div>`,
+    "supportDesc": `<div id="supportDesc" class="subsection">
+                    <p><label for="material">Material:</label> <input type="text" value="" placeholder="Write something..." id="material" name="material"></p>
+                    <p><label for="objectType">Object type:</label> <input type="text" value="" placeholder="Write something..." id="objectType" name="objectType"></p>
+                    <ul id="dimensions">Dimensions:
+                        <li><label for="height">Height:</label> <input type="text" value="" placeholder="Write something..." id="height" name="height"></li>
+                        <li><label for="width">Width:</label> <input type="text" value="" placeholder="Write something..." id="width" name="width"></li>
+                        <li><label for="depth">Depth:</label> <input type="text" value="" placeholder="Write something..." id="depth" name="depth"></li>
+                    </ul>
+                </div>`,
+    "layoutDEsc": `<div id="layoutDesc" class="subsection">
+                <p><label for="layout">Layout:</label> <input type="text" value="" placeholder="Write something..." id="layout" name="layout"></p>
+            </div>`,
+    "handDesc": `<div id="handDesc" class="subsection">
+                    <p><label for="handNote">Hand note:</label> <input type="text" value="" placeholder="Write something..." id="handNote" name="handNote"></p>
+                </div>`,
+    "scriptDesc": `<div id="scriptDesc" class="subsection">
+                    <p><label for="scriptNote">Script note:</label> <input type="text" value="" placeholder="Write something..." id="scriptNote" name="scriptNote"></p>
+                </div>`,
+    "decoDesc": `<div id="decoDesc" class="subsection">
+                        <p><label for="decoNote">Deco note:</label> <input type="text" value="" placeholder="Write something..." id="decoNote" name="decoNote"></p>
+                    </div>`,
+    "history": `<div id="history" class="subsection">
+                    <div id="origin">
+                        <p><label for="origPlace">Original place:</label> <input type="text" value="" placeholder="Write something..." id="origPlace" name="origPlace"></p>
+                        <p><label for="origDate">Orig date:</label> <input type="text" value="" placeholder="Write something..." id="origDate" name="origDate"></p>
+                    </div>
+                    <p><label for="provenance_found">Provenance (found):</label> <input type="text" value="" placeholder="Write something..." id="provenance_found" name="provenance_found"></p>
+                    <p><label for="provenance_observed">Provenance (observed):</label> <input type="text" value="" placeholder="Write something..." id="provenance_observed" name="provenance_observed"></p>
+                </div>`,
+    "facsimile": `<div id="facsimile">
+                    <p><label for="graphic">Facsimile:</label> <input type="text" value="http://www.steinheim-institut.de/daten/picsffb/xl/ffb0098.jpg" placeholder="Write something..." class="graphic" name="graphic"> <button class="graphic_button">Show</button></p>
+                </div>`,
+    "apparatus": `<div id="apparatus" class="blg">
+                    <p class="subsection">external apparatus criticus (if applicable)</p>
+                </div>`,
+    "translation": `<div id="translation" class="blg">
+                        <p class="subsection">translation(s)</p>
+                    </div>`,
+    "commentary": `<div id="commentary" class="blg">
+                        <p class="subsection">commentary</p>
+                    </div>`,
+    "bibliography": `<div id="bibliography" class="blg">
+                        <p class="subsection">bibliography of previous editions, discussion, etc.</p>
+                    </div>`
+    
+
+};
+
+
+            
---
+++ main.js
@@ -0,0 +1,251 @@
+class EED {
+    constructor(id) {
+      this.id = id;
+      this.setup();
+    }
+      
+
+    setup(){
+        const container = document.getElementById(this.id);
+        if (container == null){
+            console.log('Error: ID not found.');
+        } else {
+            const fileDesc = this.div({"id": "fileDesc", "classes": "blr"});
+            fileDesc.innerHTML += CONST.titleStmt;
+            fileDesc.innerHTML += CONST.publicationStmt;
+
+            const sourceDesc = this.div({"id": "sourceDesc", "classes": "blg"});
+            sourceDesc.innerHTML += CONST.msIdentifier;
+            sourceDesc.innerHTML += CONST.supportDesc;
+            sourceDesc.innerHTML += CONST.layoutDEsc;
+            sourceDesc.innerHTML += CONST.handDesc;
+            sourceDesc.innerHTML += CONST.scriptDesc;
+            sourceDesc.innerHTML += CONST.handDesc;
+            sourceDesc.innerHTML += CONST.history;
+
+            fileDesc.innerHTML += sourceDesc.outerHTML;
+            container.appendChild(fileDesc);
+
+            container.innerHTML += CONST.facsimile;
+            container.innerHTML += `<div class="flexbox" id="text_section">
+                                        <div id="transcription">
+                                            <div id="transcription_editor">
+                                                <p>Hello World!</p>
+                                                <p>Some initial <strong>bold</strong> text</p>
+                                                <p><br></p>
+                                            </div>
+                                        </div>
+                                        <div id="counter"></div>
+                                        <div id="img">
+                                            <div id="leaflet">
+                                        </div></div>
+                                    </div>`;
+            
+            const text_body = this.div({"id": "text_body", "classes": "blr"});
+            text_body.innerHTML += CONST.apparatus;
+            text_body.innerHTML += CONST.translation;
+            text_body.innerHTML += CONST.commentary;
+            text_body.innerHTML += CONST.bibliography;
+            container.innerHTML += text_body.outerHTML;
+
+            let image;
+
+            Array.from(document.getElementsByClassName('graphic_button')).forEach((element) => {
+                element.addEventListener('click', (e) => {
+                    
+                    const url = e.target.previousElementSibling.value.trim();
+
+                    if (url.match(/^http[s]{0,1}:\/\//i)){
+                        //document.getElementById('img').innerHTML = `<img class="facsimile" src="${url}">`;
+
+                        if(image){
+                            console.log(`Removed image ${image._url}`);
+                            image.removeFrom(mymap);
+                        }
+
+                        var bounds = [
+                            [0, 0],
+                            [100,100]
+                          ];
+
+                        image = L.imageOverlay(url, bounds, {
+                            crossOrigin: false
+                          });
+
+                        image.addTo(mymap);
+                        image.setBounds(bounds);
+                        mymap.fitBounds(bounds);
+
+                        image.addEventListener("load", (e) => {
+                            console.log(`The images natural bounds are:\n\tHeight:\t${image._image.naturalHeight} pixel\n\tWidth:\t${image._image.naturalWidth} pixel`);
+                            bounds = [
+                              [0, 0],
+                              [image._image.naturalHeight, image._image.naturalWidth]
+                            ];
+                            image.setBounds(bounds);
+                            mymap.fitBounds(bounds);
+                        });
+                        
+                    }        
+                } )
+            });
+
+            var mymap = L.map('leaflet', {
+                crs: L.CRS.Simple,
+                minZoom: -5,
+              });
+
+              //
+
+                let Inline = Quill.import('blots/inline');
+                let Block = Quill.import('blots/block');
+                let BlockEmbed = Quill.import('blots/block/embed');
+
+                //
+                // Provide markup functionality for buttons
+                //
+                class RS_Person extends Inline {
+                    static create(value) {
+                    let node = super.create();
+                    node.setAttribute("data-annotation", 'person');
+                    return node;
+                    }
+                    static formats(node) {
+                    node = {
+                        annotation: node.getAttribute("data-annotation"),
+                    };
+                    return node;
+                    }
+                }
+                // Registering
+                RS_Person.blotName = "rs-person";
+                RS_Person.tagName = "span";
+                RS_Person.className = 'rs-person';
+                Quill.register(RS_Person);
+
+                class RS_Place extends Inline {
+                    static create(value) {
+                    let node = super.create();
+                    node.setAttribute("data-annotation", 'place');
+                    return node;
+                    }
+                    static formats(node) {
+                    node = {
+                        annotation: node.getAttribute("data-annotation"),
+                    };
+                    return node;
+                    }
+                }
+                // Registering
+                RS_Place.blotName = "rs-place";
+                RS_Place.tagName = "span";
+                RS_Place.className = 'rs-place';
+                Quill.register(RS_Place);
+                
+                // BR
+                class BR extends Inline {
+                    static create(value) {
+                    let node = super.create();
+                    node.setAttribute("data-annotation", 'br');
+                    return node;
+                    }
+                    static formats(node) {
+                    node = {
+                        annotation: node.getAttribute("data-annotation"),
+                    };
+                    return node;
+                    }
+                }
+                // Registering
+                BR.blotName = "br";
+                BR.tagName = "span";
+                BR.className = 'break';
+                Quill.register(BR);
+                
+                var toolbarOptions = [['bold', 'italic', 'underline', 'strike'], ['rs-person', 'rs-place', 'br']];
+    
+
+                var quill = new Quill('#transcription_editor', {
+                    modules: {
+                        toolbar: toolbarOptions 
+                    },
+                    theme: 'snow'
+                });
+
+                var rs_person_button = document.querySelector('button.ql-rs-person');
+                rs_person_button.addEventListener('click', function() {
+                    const item = 'person';
+                    console.log(quill.getFormat().SPAN);
+                    if (quill.getFormat().SPAN === undefined) {
+                        console.log("undef");
+                        console.log(quill.getSelection());
+                        
+                        let { index, length } = quill.getSelection();
+                        quill.format("SPAN", [item, index, index + length]);
+                      } else if (quill.getFormat().SPAN.annotation === item) {
+                        let { index, length } = quill.getSelection();
+                        quill.removeFormat(index, length);
+                      } else {
+                        let { index, length } = quill.getSelection();
+                        quill.format("SPAN", [item, index, index + length]);
+                      }
+                });
+
+                var rs_place_button = document.querySelector('button.ql-rs-place');
+                rs_place_button.addEventListener('click', function() {
+                    const item = 'place';
+                    console.log(quill.getFormat().SPAN);
+                    if (quill.getFormat().SPAN === undefined) {
+                        console.log("undef");
+                        console.log(quill.getSelection());
+                        
+                        let { index, length } = quill.getSelection();
+                        quill.format("SPAN", [item, index, index + length]);
+                      } else if (quill.getFormat().SPAN.annotation === item) {
+                        let { index, length } = quill.getSelection();
+                        quill.removeFormat(index, length);
+                      } else {
+                        let { index, length } = quill.getSelection();
+                        quill.format("SPAN", [item, index, index + length]);
+                      }
+                });
+
+                var br_button = document.querySelector('button.ql-br');
+                br_button.addEventListener('click', function() {
+                    const item = 'br';
+                    console.log(quill.getFormat().SPAN);
+                    if (quill.getFormat().SPAN === undefined) {
+                        console.log("undef");
+                        console.log(quill.getSelection());
+                        
+                        let { index, length } = quill.getSelection();
+                        quill.format("SPAN", [item, index, index]);
+                      } else if (quill.getFormat().SPAN.annotation === item) {
+                        let { index, length } = quill.getSelection();
+                        quill.removeFormat(index, length);
+                      } else {
+                        let { index, length } = quill.getSelection();
+                        quill.format("SPAN", [item, index, index]);
+                      }
+                });
+
+              
+        }
+    }
+
+
+    div(config){
+        const div = document.createElement('DIV');
+        if (config.id){
+            div.id = config.id;
+        }
+        if (config.classes){
+            div.className = config.classes;
+        }
+        return div 
+    }
+  }
+
+
+  var eed = new EED('epidoc'); 
+
