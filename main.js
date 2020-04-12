9dedd451ba4e83bb720a94f154da0b64a95059d0
Max Grüntgens <max.gruentgens@adwmainz.de>
2020-04-12 17:35:07 +0200

    Autosuggest iconclass entries on decoNote fields.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ class EED {
     constructor(id) {
       this.id = id;
       this.setup();
+      this.eventListeners();
     }
       
 
@@ -20,7 +21,7 @@ class EED {
             sourceDesc.innerHTML += CONST.layoutDEsc;
             sourceDesc.innerHTML += CONST.handDesc;
             sourceDesc.innerHTML += CONST.scriptDesc;
-            sourceDesc.innerHTML += CONST.handDesc;
+            sourceDesc.innerHTML += CONST.decoDesc;
             sourceDesc.innerHTML += CONST.history;
 
             fileDesc.innerHTML += sourceDesc.outerHTML;
@@ -231,6 +232,40 @@ class EED {
 
               
         }
+    }
+
+    eventListeners(){
+        Array.from(document.getElementsByClassName('decoNote')).forEach((item) => {
+            item.addEventListener('input', function(e){
+                const input = this.value;
+                var re = new RegExp(input, 'i');
+                const sublist = DATA.iconclass.filter((item) => {
+                    return item.match(re); 
+                }).map((item) => {
+                    return `<p style="background: lightgrey;">${item}</p>`
+                }).join('\n');
+
+                //console.log(sublist);
+                
+                if(!this.nextElementSibling) {
+                    const iconclass_suggestions = document.createElement('DIV');
+                    iconclass_suggestions.id = "iconclass";
+                    iconclass_suggestions.innerHTML = sublist;
+                    this.parentElement.appendChild(iconclass_suggestions);
+                } else {
+                    this.nextElementSibling.innerHTML = sublist;
+                }
+
+                const sublist_items = Array.from(document.querySelectorAll('div#iconclass > p'));
+                sublist_items.forEach((item) => {
+                    item.addEventListener('click', (e) => {
+                        this.value = item.textContent;
+                        this.nextElementSibling.innerHTML = '';
+                    })
+                });
+                
+            });
+        })
     }
 
 
