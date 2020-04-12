e3658316fb4094a7d23e07f2372d592b02abf97a
Max Grüntgens <max.gruentgens@adwmainz.de>
2020-04-12 19:14:13 +0200

    Pleiades placename lookup via autosuggest.

--- main.js
+++ main.js
@@ -285,6 +285,37 @@ class EED {
 
             });
         })
+
+
+        document.getElementById('origPlace').addEventListener('input', function(e){
+                const input = this.value;
+                var re = new RegExp(input, 'i');
+                const sublist = PLEIADES.filter((item) => {
+                    return item.name.match(re); 
+                }).map((item) => {
+                    return `<p style="background: lightgrey;">${item.name}</p>`
+                }).join('\n');
+
+                //console.log(sublist);
+                
+                if(!this.nextElementSibling) {
+                    const iconclass_suggestions = document.createElement('DIV');
+                    iconclass_suggestions.className = "pleiades autosuggest";
+                    iconclass_suggestions.innerHTML = sublist;
+                    this.parentElement.appendChild(iconclass_suggestions);
+                } else {
+                    this.nextElementSibling.innerHTML = sublist;
+                }
+
+                const sublist_items = Array.from(this.nextElementSibling.querySelectorAll('p'));
+                sublist_items.forEach((item) => {
+                    item.addEventListener('click', (e) => {
+                        this.value = item.textContent;
+                        this.nextElementSibling.innerHTML = '';
+                    })
+                });
+
+            });
     }
 
 
